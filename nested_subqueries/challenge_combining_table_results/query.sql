1a98ede13aa2140c38aee0243ec0e8b6424b4136
Elainesoh <>
2026-02-02 03:37:43 +0000

    Challenge: Combining Table Results

---
+++ nested_subqueries/challenge_combining_table_results/query.sql
@@ -0,0 +1,11 @@
+SELECT id, first_name, last_name
+FROM (
+  -- write an inner subquery here 
+    SELECT employee_id as id, first_name, last_name 
+    FROM employees
+    UNION 
+    SELECT contractor_id as id, first_name, last_name 
+    FROM contractors
+  ) AS combined
+-- don't forget to write the WHERE clause here
+WHERE id % 2 = 0 
